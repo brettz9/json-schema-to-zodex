be520ed9ca1e4aef5879838b2dd3bbc362b7b47e
Brett Zamir <brettz9@yahoo.com>
2025-11-20 22:15:21 -0700

    fix: simplify oneOf

--- src/parsers/parseOneOf.ts
+++ src/parsers/parseOneOf.ts
@@ -6,36 +6,9 @@ export const parseOneOf = (
   refs: Refs,
 ) => {
   return schema.oneOf.length
-    ? schema.oneOf.length === 1
-      ? parseSchema(schema.oneOf[0], {
-          ...refs,
-          path: [...refs.path, "oneOf", 0],
-        })
-      : `z.any().superRefine((x, ctx) => {
-    const schemas = [${schema.oneOf
-      .map((schema, i) =>
-        parseSchema(schema, {
-          ...refs,
-          path: [...refs.path, "oneOf", i],
-        }),
-      )
-      .join(", ")}];
-    const errors = schemas.reduce<z.ZodError[]>(
-      (errors, schema) =>
-        ((result) =>
-          result.error ? [...errors, result.error] : errors)(
-          schema.safeParse(x),
-        ),
-      [],
-    );
-    if (schemas.length - errors.length !== 1) {
-      ctx.addIssue({
-        path: ctx.path,
-        code: "invalid_union",
-        unionErrors: errors,
-        message: "Invalid input: Should pass single schema",
-      });
-    }
-  })`
+    ? parseSchema(schema.oneOf[0], {
+      ...refs,
+      path: [...refs.path, "oneOf", 0],
+    })
     : `{"type": "any"}`;
 };
--- test/parsers/parseOneOf.test.ts
+++ test/parsers/parseOneOf.test.ts
@@ -2,41 +2,6 @@ import { parseOneOf } from "../../src/parsers/parseOneOf";
 import { suite } from "../suite";
 
 suite("parseOneOf", (test) => {
-  // test("should create a union from two or more schemas", (assert) => {
-  //   assert(
-  //     parseOneOf(
-  //       {
-  //         oneOf: [
-  //           {
-  //             type: "string",
-  //           },
-  //           { type: "number" },
-  //         ],
-  //       },
-  //       { path: [], seen: new Map() },
-  //     ),
-  //     `z.any().superRefine((x, ctx) => {
-  //   const schemas = [z.string(), z.number()];
-  //   const errors = schemas.reduce<z.ZodError[]>(
-  //     (errors, schema) =>
-  //       ((result) =>
-  //         result.error ? [...errors, result.error] : errors)(
-  //         schema.safeParse(x),
-  //       ),
-  //     [],
-  //   );
-  //   if (schemas.length - errors.length !== 1) {
-  //     ctx.addIssue({
-  //       path: ctx.path,
-  //       code: "invalid_union",
-  //       unionErrors: errors,
-  //       message: "Invalid input: Should pass single schema",
-  //     });
-  //   }
-  // })`,
-  //   );
-  // });
-
   test("should extract a single schema", (assert) => {
     assert(
       parseOneOf(
